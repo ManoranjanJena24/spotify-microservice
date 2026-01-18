62182b886bbcf1244663b3dfa880dc7b430d4e4b
Manoranjan Jena <manoranjanjenamitsy17@gmail.com>
2026-01-18 22:30:25 +0530

    Created the Side bar

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -6,6 +6,7 @@
 // }
 
 import { useNavigate } from "react-router-dom";
+import PlayListCard from "./PlayListCard";
 
 // export default Sidebar
 
@@ -23,11 +24,35 @@ const Sidebar = () => {
          <img src="/home.png" alt="" className="w-6"/>
           <p className="font-bold">Home</p>
         </div>
+        <div 
+          className="flex items-center gap-3 pl-8 cursor-pointer">
+         <img src="/search.png" alt="" className="w-6"/>
+          <p className="font-bold">Search</p>
+        </div>
+       
       </div>
       
       {/* Niche wala Library section yahan add kar sakte ho */}
       <div className="bg-[#121212] h-[85%] rounded mt-2">
          {/* Library Content */}
+         <div className="p-4 flex items-center justify-between">
+            <div className="flex items-center gap-3">
+              <img src="/stack.png" className="w-8" alt="" />
+              <p className="font-semibold"> Your Library</p>
+            </div>
+            <div className="flex items-center gap-3">
+              <img src="/arrow.png" className="w-8" alt="" />
+              <img src="/plus.png" className="w-8" alt="" />
+            </div>
+         </div>
+         <div onClick={()=>navigate("/playlist")}>
+          <PlayListCard/>
+         </div>
+         <div className="p-4 m-2 bg-[#121212] rounded font-semibold flex-col items-start gap-1 pl-4 mt-4" >
+          <h1>Let's findsome podcasts to follow</h1>
+          <p className="font-light">we'll keep you update on new episodes</p>
+          <button className="px-4 py-1.5 bg-white text-black text-[15px] rounded-full mt-4">Browse Podcast</button>
+         </div>
       </div>
     </div>
   );
